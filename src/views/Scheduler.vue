14ec2e177ee7bdbed68ec09b1ae828567f880b4f
bruslaan <bruslan.no@mail.com>
2020-02-06 18:25:00 +0100

    Schedular profile picture added

--- src/views/Scheduler.vue
+++ src/views/Scheduler.vue
@@ -14,9 +14,14 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(n,i) in 60" :key="i">
-            <th>Peter Zwegert</th>
-            <td v-for="(n,i) in 31" :key="i"></td>
+          <tr v-for="(element,i) in Mitarbeiter" :key="i">
+            <th class="name_header">
+              <v-avatar class="mr-1" size="36">
+                <img :src="`https://i.pravatar.cc/10${i}`" alt="John" />
+              </v-avatar>
+              {{element.name}}
+            </th>
+            <td v-for="(n,i) in 31" :key="i" :class="[ hasActiveElements(element, n) ]"></td>
           </tr>
         </tbody>
       </template>
@@ -26,9 +31,101 @@
 
 <script scoped>
 export default {
+  computed: {},
+  methods: {
+    hasActiveElements(element, n) {
+      return n >= element.abwesenheit.split("-")[0] &&
+        n <= element.abwesenheit.split("-")[1]
+        ? { scheduler_active: true }
+        : {};
+    }
+  },
   data() {
     return {
-      desserts: []
+      Mitarbeiter: [
+        {
+          name: "Hans Peter Hans Maraschki",
+          abwesenheit: "12-20"
+        },
+        {
+          name: "Peter Zwegert",
+          abwesenheit: "2-5"
+        },
+        {
+          name: "Jürgen Meier",
+          abwesenheit: "1-7"
+        },
+        {
+          name: "Jürgen Maraschki",
+          abwesenheit: "15-19"
+        },
+        {
+          name: "Ruslan Montenari",
+          abwesenheit: "4-7"
+        },
+                {
+          name: "Peter Zwegert",
+          abwesenheit: "2-5"
+        },
+        {
+          name: "Jürgen Meier",
+          abwesenheit: "1-7"
+        },
+        {
+          name: "Jürgen Maraschki",
+          abwesenheit: ""
+        },
+        {
+          name: "Ruslan Montenari",
+          abwesenheit: "14-17"
+        },
+                {
+          name: "Peter Zwegert",
+          abwesenheit: "2-10"
+        },
+        {
+          name: "Jürgen Meier",
+          abwesenheit: "1-7"
+        },
+        {
+          name: "Jürgen Maraschki",
+          abwesenheit: "15-19"
+        },
+        {
+          name: "Ruslan Montenari",
+          abwesenheit: "4-7"
+        },        {
+          name: "Peter Zwegert",
+          abwesenheit: "2-5"
+        },
+        {
+          name: "Jürgen Meier",
+          abwesenheit: "1-7"
+        },
+        {
+          name: "Jürgen Maraschki",
+          abwesenheit: "15-19"
+        },
+        {
+          name: "Ruslan Montenari",
+          abwesenheit: "14-17"
+        },        {
+          name: "Peter Zwegert",
+          abwesenheit: "2-15"
+        },
+        {
+          name: "Jürgen Meier",
+          abwesenheit: "10-17"
+        },
+        {
+          name: "Jürgen Maraschki",
+          abwesenheit: "15-19"
+        },
+        {
+          name: "Ruslan Montenari",
+          abwesenheit: "4-7"
+        }
+      ]
     };
   }
 };
@@ -38,6 +135,10 @@ export default {
 
 
 <style scoped>
+.scheduler_active {
+  background: lightblue;
+  border-color: lightblue;
+}
 .tcontainer {
   /* max-width: 100vh;
   max-height: 100vh;
@@ -46,8 +147,10 @@ export default {
   margin: 10px;
 }
 .name_header {
-  min-width: 150px;
+  min-width: 250px;
+  text-align: left !important;
 }
+
 table {
   position: relative;
   border-collapse: collapse;
