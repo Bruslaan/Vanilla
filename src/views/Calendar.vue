eccce7f13f969f59473131a8ddd13aac6343e94a
Francesco Montanari <francesco@francescos-mbp.speedport_w_921v_1_46_000>
2020-03-22 10:31:12 +0100

    calendar monat bestätigt visualisiert & keine eintragung möglich

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -3,7 +3,10 @@
     <!-- SnackBar -->
     <v-snackbar color="red" v-model="snackbar">
       {{ text }}
-      <v-btn color="white" text @click="snackbar = false">Close</v-btn>
+      <v-btn text fab dark small @click="snackbar = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <!-- <v-btn color="white" text @click="snackbar = false">Schließen</v-btn> -->
     </v-snackbar>
     <!-- Floating Button -->
     <v-btn
@@ -19,33 +22,48 @@
     </v-btn>
     <!-- TOOLBAR -->
     <v-col class="pa-0">
-        <v-toolbar flat color="white">
-          <v-btn fab text small color="grey darken-2" @click="prev">
-            <v-icon small>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-btn text color="grey darken-2" @click="setToday">Today</v-btn>
-          <v-btn fab text small color="grey darken-2" @click="next">
-            <v-icon small>mdi-chevron-right</v-icon>
-          </v-btn>
-          <h1 class="ml-2">{{ title }}</h1>
-          <v-spacer></v-spacer>
-          <v-menu bottom right>
-            <template v-slot:activator="{ on }">
-              <v-btn outlined color="grey darken-2" v-on="on">
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>mdi-menu-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
+      <v-toolbar flat color="white">
+        <v-btn fab text small color="grey darken-2" @click="prev">
+          <v-icon small>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn text color="grey darken-2" @click="setToday">HEUTE</v-btn>
+        <v-btn fab text small color="grey darken-2" @click="next">
+          <v-icon small>mdi-chevron-right</v-icon>
+        </v-btn>
+        <h1 class="ml-2">{{ title }}</h1>
+        <v-chip
+          v-if="ifMonatBestätigt(start)"
+          class="ma-2 ml-3"
+          small
+          color="green"
+          text-color="white"
+        >Bestätigt</v-chip>
+        <h1 class="ml-2">{{ title2 }}</h1>
+        <v-chip
+          v-if="ifMonatBestätigt(end)"
+          class="ma-2 ml-3"
+          small
+          color="green"
+          text-color="white"
+        >Bestätigt</v-chip>
+        <v-spacer></v-spacer>
+        <v-menu bottom right>
+          <template v-slot:activator="{ on }">
+            <v-btn outlined color="grey darken-2" v-on="on">
+              <span>{{ typeToLabel[type] }}</span>
+              <v-icon right>mdi-menu-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="type = 'week'">
+              <v-list-item-title>Week</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="type = 'month'">
+              <v-list-item-title>Month</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar>
       <!-- CALENDAR -->
       <v-sheet height="80vh">
         <v-calendar
@@ -201,6 +219,11 @@ export default {
     eventToRender: null,
     eventHovered: false,
     // events are all events of an user and all blockierungen
+    monatBestätigt: {
+      "2020-03": true,
+      "2020-04": true,
+      "2020-05": true
+    },
     events: [
       {
         name: "Blockierung",
@@ -225,6 +248,15 @@ export default {
         color: "#ff4500",
         type: "Blockierung",
         status: "bestätigt"
+      },
+      {
+        name: "Urlaub",
+        start: "2020-02-15",
+        end: "2020-02-16",
+        color: "#FFB63D",
+        type: "Abwesenheit",
+        status: "storniert",
+        abwesenheitsGrund: "Urlaub"
       },
       {
         name: "Urlaub",
@@ -255,7 +287,7 @@ export default {
       },
       {
         name: "Arbeit",
-        start: "2020-02-21 07:00",
+        start: "2020-02-21 02:00",
         end: "2020-02-21 11:00",
         color: "#C5C5C5",
         type: "Anwesenheit",
@@ -265,7 +297,7 @@ export default {
       {
         name: "Arbeit",
         start: "2020-02-21 12:00",
-        end: "2020-02-21 15:30",
+        end: "2020-02-21 23:20",
         color: "#C5C5C5",
         type: "Anwesenheit",
         status: "angefragt",
@@ -315,9 +347,9 @@ export default {
       }
 
       const startMonth = this.monthFormatter(start);
-
       const startYear = start.year;
       const startDay = start.day;
+
       switch (this.type) {
         case "month":
           return `${startMonth} ${startYear}`;
@@ -325,6 +357,23 @@ export default {
           return `${startMonth} ${startYear}`;
         case "day":
           return `${startMonth} ${startDay} ${startYear}`;
+      }
+      return "";
+    },
+    title2() {
+      const { start, end } = this;
+      if (!start || !end) {
+        return "";
+      }
+      const endMonth = this.monthFormatter(end);
+      const endYear = end.year;
+      const startMonth = this.monthFormatter(start);
+
+      switch (this.type) {
+        case "week":
+          if (startMonth != endMonth) {
+            return `- ${endMonth} ${endYear}`;
+          }
       }
       return "";
     },
@@ -340,6 +389,33 @@ export default {
     this.$refs.calendar.checkChange();
   },
   methods: {
+    ifMonatBestätigt(datum) {
+      if (datum) {
+        let key = "";
+        const endMonth = this.monthFormatter(this.end);
+        const startMonth = this.monthFormatter(this.start);
+        if (datum == this.start) {
+          const Year = String(this.start.year);
+          const Month = String(this.start.month).padStart(2, "0");
+          key = Year + "-" + Month;
+          if (this.monatBestätigt[key] == true) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+        if (datum == this.end && startMonth != endMonth) {
+          const Year = String(this.end.year);
+          const Month = String(this.end.month).padStart(2, "0");
+          key = Year + "-" + Month;
+          if (this.monatBestätigt[key] == true) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    },
     createEvent() {
       // this.selectedEvent.type="Abwesenheit"
       // this.selectedEvent.abwesenheitsGrund = "Urlaub"
@@ -500,11 +576,11 @@ export default {
           let startTime = Date.parse(this.events[index]["start"] + " 00:00");
           let endTime = Date.parse(this.events[index]["end"] + " 23:59");
           if (newStartTime <= startTime && newEndTime > startTime) {
-            console.log("olga1")
+            console.log("olga1");
             return true;
           }
           if (newStartTime > startTime && newStartTime < endTime) {
-            console.log("olga2")
+            console.log("olga2");
             return true;
           }
         }
@@ -520,7 +596,6 @@ export default {
         this.snackbar = true;
         return true;
       }
-
       if (!this.selectedEvent.type) {
         this.text = "Erfassung nicht ausgewählt";
         this.snackbar = true;
@@ -557,12 +632,28 @@ export default {
         this.snackbar = true;
         return true;
       }
-
+      // if (
+      //   this.selectedEvent.type == "Anwesenheit" &&
+      //   this.selectedEvent.startTime
+      // )
+      let refStart =
+        this.selectedEvent.startDay.split("-")[0] +
+        "-" +
+        this.selectedEvent.startDay.split("-")[1];
+      let refEnd =
+        this.selectedEvent.endDay.split("-")[0] +
+        "-" +
+        this.selectedEvent.endDay.split("-")[1];
       if (
-        this.selectedEvent.type == "Anwesenheit" &&
-        this.selectedEvent.startTime
-      )
-        return false;
+        this.monatBestätigt[refStart] == true ||
+        this.monatBestätigt[refEnd] == true
+      ) {
+        this.text = "Dieser Monat wurde bereits bestätigt";
+        this.snackbar = true;
+        return true;
+      }
+
+      return false;
     },
 
     returnColor() {
